6f5af95f5f815317d9c4541600179f6b3ef51590
Spencer Corkran <corkransp@gmail.com>
2019-02-20 08:59:50 -0500

    checkpoint model
    10M and 50M versions

---
+++ Assets/Jonah & Spencer scripts/Pickup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(15,30,45) * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+            Destroy(this.gameObject);
+    }
+}
